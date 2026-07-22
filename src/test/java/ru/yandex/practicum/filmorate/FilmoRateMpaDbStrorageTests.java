ae48e904e8e0088392f33e51801fd96c359ce745
Radmir Nurgaliev <nradmir@gmail.com>
2026-07-22 20:52:44 +0300

    fix: исправлены ошибки checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/FilmoRateMpaDbStrorageTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmoRateMpaDbStrorageTests.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
-import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
-import ru.yandex.practicum.filmorate.storage.genre.GenreDbStorage;
 import ru.yandex.practicum.filmorate.storage.mpa.MpaDbStorage;
 
 import java.util.Collection;
